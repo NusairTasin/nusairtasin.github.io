b99a92e4c819bf86b9b2db7e61aa1367abff34b8
NusairTasin <tasinnusair@gmail.com>
2024-11-02 20:35:14 +0600

    Contact Me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,14 @@
-
+import {motion} from 'framer-motion'
 const Contact = () => {
   return (
-    <div id='contact' className='flex flex-col gap-3 justify-center items-center w-full min-h-[80vh]'>
-        <h1 className="text-5xl bg-gradient-to-r from-blue-500 to-blue-700 text-transparent bg-clip-text ">Get in Touch</h1>
-        <p className='font-semibold text-slate-500'>Want to chat? Send me an E-mail through this button and I'll respond whenever I can.</p>
-        <button className='text-white bg-gradient-to-r from-blue-500 to-pink-700 p-3 rounded-2xl opacity-85 transition-all duration-300 hover:scale-105 hover:opacity-100'
+    <motion.div
+        initial={{opacity:0, y:100}}
+        whileInView={{opacity:1, y:0}}
+        transition={{duration:2}}
+    id='contact' className='flex flex-col gap-10 justify-center items-center w-full min-h-[80vh]'>
+        <h1 className="md:text-5xl text-4xl bg-gradient-to-r from-blue-500 to-blue-700 text-transparent bg-clip-text ">Get in Touch</h1>
+        <p className='text-center items-center justify-center flex md:text-xl text-lg font-semibold text-slate-500'>Want to chat? Send me an E-mail through this button and I'll respond whenever I can.</p>
+        <button className=' text-white bg-gradient-to-r from-blue-500 to-pink-700 p-3 rounded-2xl opacity-85 transition-all duration-300 hover:scale-105 hover:opacity-100'
         style={{
             backgroundSize: "300% 300%",
             animation: 'gradient-animation 5s ease infinite'
@@ -26,7 +30,7 @@ const Contact = () => {
                 `}
             </style>
         </button>
-    </div>
+    </motion.div>
   )
 }
 
